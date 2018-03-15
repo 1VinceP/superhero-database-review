634463413892e55d5d4b3b989f3c32cd268a7ced
Vincent Palmer <vpalmergraphics@gmail.com>
2018-03-15 16:27:37 -0600

    added header component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import marvel from './marvel.png';
 import dc from './dc.png';
 import './App.css';
 
+import Header from './components/Header';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <header>
-          <div>Secret Superhero (Data)base</div>
-        </header>
+        <Header />
         
         <div className='app-body'>
           <img src={marvel} alt='Marvel' />
